7eb722ae51f3fa738ffbfeed0cf4282cd85f1e59
Artur Opala <artur.opala@siili.fi>
2016-12-22 00:25:28 +0100

    implement step 2

--- src/main/scala/Exercise.scala
+++ src/main/scala/Exercise.scala
@@ -3,33 +3,72 @@ package exercise
 class Shop[Item, Price: Numeric] {
 
     type PriceList = Item => Option[Price]
-    type Entry = (Item, Option[Price])
+    type Entry = (Item, Price)
+    type SpecialOffer = (List[Entry], Price) => (List[Entry], Price)
 
     case class Bill(entries: List[Entry], total: Price)
-    case class CheckoutFailure(issues: Set[Item], entries: List[Entry])
+    case class CheckoutFailure(issues: Set[Item], items: List[Item])
 
     val op = implicitly[Numeric[Price]]
 
-    class Cashdesk(priceOf: PriceList) {
+    def sumTotal(entries: List[Entry]): Price = entries.map(_._2).foldLeft(op.zero)(op.plus)
+
+    class Cashdesk(priceOf: PriceList, applyOffer: SpecialOffer = (e, t) => (e, t)) {
 
         def checkout(items: List[Item]): Either[CheckoutFailure, Bill] = {
 
-            val entries: List[Entry] = items.map(item => (item, priceOf(item)))
-            val totalOpt: Option[Price] = entries.map(_._2)
-                .foldLeft(Option(op.zero))((so, po) => for (s <- so; p <- po) yield op.plus(s, p))
+            val result: Either[Set[Item], List[Entry]] = {
+                val entries: List[(Item, Option[Price])] = items.map(item => (item, priceOf(item)))
+                val successes: List[Entry] = entries.collect { case (i, Some(p)) => (i, p) }
+                if (successes.size == entries.size) {
+                    Right(successes)
+                }
+                else {
+                    val issues: Set[Item] = entries.collect({ case (i, None) => i }).toSet
+                    Left(issues)
+                }
+            }
 
-            totalOpt match {
-                case Some(total) =>
-                    Right(Bill(entries, total))
+            result match {
+                case Right(entries) =>
+                    val (es, t) = applyOffer(entries, sumTotal(entries))
+                    Right(Bill(es, t))
 
-                case None =>
-                    val issues = entries.collect { case (item, None) => item }.toSet
-                    Left(CheckoutFailure(issues, entries))
+                case Left(issues) =>
+                    Left(CheckoutFailure(issues, items))
+            }
+        }
+
+    }
+
+    object Offers {
+
+        private[this] def is(item: Item)(entry: Entry) = entry._1 == item
+        private[this] def free(entry: Entry): Entry = (entry._1, op.zero)
+        private[this] def discount(n: Int)(entries: List[Entry]): List[Entry] = {
+            val (a, b) = entries.splitAt(entries.size - n)
+            a ++ b.map(free)
+        }
+
+        def buyOneGetOneFree(item: Item): SpecialOffer =
+            (entries: List[Entry], total: Price) => {
+                val freeEntries = entries.filter(is(item)).map(free)
+                (entries ++ freeEntries, total)
+            }
+
+        def nItemsForThePriceOfM(item: Item, n: Int, m: Int): SpecialOffer = {
+            require(m > 0, "number of discounted items must be greater than 0")
+            require(m < n, "item's group size must be larger than number of discounted items")
+            (entries: List[Entry], total: Price) => {
+                val (eligible, others) = entries.partition(is(item))
+                val discounted = eligible.grouped(n).map(g => if (g.size < n) g else discount(n - m)(g)).flatten.toList
+                val all = discounted ++ others
+                (all, sumTotal(all))
             }
         }
 
     }
 }
 
-object AppleOrangeShop extends Shop[String, BigDecimal]
+object SimpleShop extends Shop[String, BigDecimal]
 
--- src/test/scala/ExerciseSpec.scala
+++ src/test/scala/ExerciseSpec.scala
@@ -5,19 +5,23 @@ import org.scalatest.prop.PropertyChecks
 import org.scalacheck._
 
 class SomeSpec extends WordSpecLike with Matchers with PropertyChecks with Inside {
+    import SimpleShop._
 
-    implicit override val generatorDrivenConfig = PropertyCheckConfig(minSize = 1, maxSize = 100, minSuccessful = 100, workers = 5)
+    implicit override val generatorDrivenConfig = PropertyCheckConfig(minSize = 0, maxSize = 100, minSuccessful = 100, workers = 5)
 
-    val appleListGen: Gen[List[String]] = Gen.listOf(Gen.const("Apple"))
-    val orangeListGen: Gen[List[String]] = Gen.listOf(Gen.const("Orange"))
-    val bananaListGen: Gen[List[String]] = Gen.listOf(Gen.const("Banana"))
+    val Apple = "Apple"
+    val Orange = "Orange"
+    val Banana = "Banana"
+
+    val appleListGen: Gen[List[String]] = Gen.listOf(Gen.const(Apple))
+    val orangeListGen: Gen[List[String]] = Gen.listOf(Gen.const(Orange))
+    val bananaListGen: Gen[List[String]] = Gen.listOf(Gen.const(Banana))
     val otherListGen: Gen[List[String]] = Gen.listOf(Gen.alphaStr)
 
-    "AppleOrangeShop" must {
+    val pricelist = Map[String, BigDecimal](Apple -> 0.60, Orange -> 0.25)
 
-        import AppleOrangeShop._
+    "SimpleShop" must {
 
-        val pricelist = Map[String, BigDecimal]("Apple" -> 0.60, "Orange" -> 0.25)
         val cashdesk = new Cashdesk(pricelist.get)
 
         "checkout empty list of items" in {
@@ -26,24 +30,27 @@ class SomeSpec extends WordSpecLike with Matchers with PropertyChecks with Insid
                 case Right(Bill(entries, total)) =>
                     total shouldBe 0
                     entries should have size (0)
+                case Left(CheckoutFailure(issues, items)) => fail
             }
         }
 
         "checkout list of known items" in {
-            val items = List("Apple", "Orange")
+            val items = List(Apple, Orange)
             inside(cashdesk.checkout(items)) {
                 case Right(Bill(entries, total)) =>
                     total shouldBe 0.85
                     entries should have size (2)
+                case Left(CheckoutFailure(issues, items)) => fail
             }
         }
 
         "not checkout list containing unknown item" in {
-            val items = List("Apple", "Orange", "Banana")
+            val items = List(Apple, Orange, Banana)
             inside(cashdesk.checkout(items)) {
-                case Left(CheckoutFailure(issues, entries)) =>
-                    issues shouldBe Set("Banana")
-                    entries should have size (3)
+                case Left(CheckoutFailure(issues, items)) =>
+                    issues shouldBe Set(Banana)
+                    items should have size (3)
+                case Right(Bill(entries, total)) => fail
             }
         }
 
@@ -53,8 +60,9 @@ class SomeSpec extends WordSpecLike with Matchers with PropertyChecks with Insid
                     val items = apples ++ oranges
                     inside(cashdesk.checkout(items)) {
                         case Right(Bill(entries, total)) =>
-                            total shouldBe (apples.size * pricelist("Apple") + oranges.size * (pricelist("Orange")))
+                            total shouldBe (apples.size * pricelist(Apple) + oranges.size * pricelist(Orange))
                             entries should have size (apples.size + oranges.size)
+                        case Left(CheckoutFailure(issues, items)) => fail
                     }
             }
         }
@@ -65,15 +73,68 @@ class SomeSpec extends WordSpecLike with Matchers with PropertyChecks with Insid
                     whenever(bananas.size > 0 && others.size > 0) {
                         val items = apples ++ oranges ++ bananas ++ others
                         inside(cashdesk.checkout(items)) {
-                            case Left(CheckoutFailure(issues, entries)) =>
-                                issues should contain("Banana")
+                            case Left(CheckoutFailure(issues, items)) =>
+                                issues should contain(Banana)
                                 for (other <- others) issues should contain(other)
-                                entries should have size (apples.size + oranges.size + bananas.size + others.size)
+                                items should have size (apples.size + oranges.size + bananas.size + others.size)
+                            case Right(Bill(entries, total)) => fail
                         }
                     }
             }
         }
 
+    }
+
+    "Offers.buyOneGetOneFree" must {
+
+        "add free apple for one existing" in {
+            val entries: List[Entry] = List((Apple, 0.60))
+            Offers.buyOneGetOneFree(Apple)(entries, 125) shouldBe (List((Apple, 0.60), (Apple, 0)), 125)
+        }
+
+        "add free apple for each one existing" in {
+            val entries: List[Entry] = List((Apple, 0.60), (Apple, 0.60))
+            Offers.buyOneGetOneFree(Apple)(entries, 125) shouldBe (List((Apple, 0.60), (Apple, 0.60), (Apple, 0), (Apple, 0)), 125)
+        }
+
+        "add free apple for each one existing but not for orange" in {
+            val entries: List[Entry] = List((Apple, 0.60), (Orange, 0.25), (Apple, 0.60))
+            Offers.buyOneGetOneFree(Apple)(entries, 125) shouldBe (List((Apple, 0.60), (Orange, 0.25), (Apple, 0.60), (Apple, 0), (Apple, 0)), 125)
+        }
+
+        "return unmodified if don't contain apples" in {
+            val entries: List[Entry] = List((Orange, 0.25))
+            Offers.buyOneGetOneFree(Apple)(entries, 125) shouldBe (List((Orange, 0.25)), 125)
+        }
+    }
+
+    "Offers.nItemsForThePriceOfM" must {
+
+        "not discount when number of items less then threshold" in {
+            val entries: List[Entry] = List((Orange, 0.25), (Orange, 0.25))
+            Offers.nItemsForThePriceOfM(Orange, 3, 2)(entries, 2) shouldBe (List((Orange, 0.25), (Orange, 0.25)), 0.5)
+        }
+
+        "discount third orange and recalculate price" in {
+            val entries: List[Entry] = List((Orange, 0.25), (Orange, 0.25), (Orange, 0.25))
+            Offers.nItemsForThePriceOfM(Orange, 3, 2)(entries, 0.75) shouldBe (List((Orange, 0.25), (Orange, 0.25), (Orange, 0)), 0.5)
+        }
+
+        "discount each third orange" in {
+            val entries: List[Entry] = List((Orange, 0.25), (Orange, 0.25), (Orange, 0.25), (Orange, 0.25), (Orange, 0.25))
+            Offers.nItemsForThePriceOfM(Orange, 3, 2)(entries, 0) shouldBe (List((Orange, 0.25), (Orange, 0.25), (Orange, 0), (Orange, 0.25), (Orange, 0.25)), 1.0)
+        }
+
+        "not change number of entries and recalculate price" in {
+            forAll(appleListGen, orangeListGen) {
+                (apples, oranges) =>
+                    val entries = (apples ++ oranges).map(i => (i, pricelist(i)))
+                    val (modified, total) = Offers.nItemsForThePriceOfM(Orange, 3, 2)(entries, 0)
+                    modified.size shouldBe entries.size
+                    val o = (oranges.size - op.toInt(oranges.size / 3))
+                    total shouldBe (o * pricelist(Orange) + apples.size * pricelist(Apple))
+            }
+        }
     }
 
 }
